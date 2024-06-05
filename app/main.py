fa43606ee6b9c1b32f5a59d036e9999ab2ae638b
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-06 00:10:55 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,26 +1,19 @@
 import subprocess
 import sys
+import tempfile
+import shutil
 import os
-
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    #print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[3]
     args = sys.argv[4:]
-    #
-    os.system("mkdir -p /yinh/usr/local/bin")
-    os.system("cp /usr/local/bin/docker-explorer /yinh/usr/local/bin")
-    os.chroot("/yinh")
+    tmp_dir = tempfile.mkdtemp()
+    shutil.copy(command, tmp_dir)
+    os.chroot(tmp_dir)
+    command = os.path.join("/", os.path.basename(command))
     completed_process = subprocess.run([command, *args], capture_output=True)
-    #print(completed_process.stdout.decode("utf-8"))
-    sys.stdout.buffer.write(completed_process.stdout)
-    sys.stderr.buffer.write(completed_process.stderr)
-    sys.exit(completed_process.returncode)
-
-
+    sys.stdout.write(completed_process.stdout.decode("utf-8"))
+    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    exit(completed_process.returncode)
 if __name__ == "__main__":
     main()
