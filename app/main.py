e2452556527f46a633d16d9e0bbce6053b9d7551
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-06 00:24:13 +0530

    test

--- app/main.py
+++ app/main.py
@@ -12,9 +12,6 @@ def unshare(flags):
         raise OSError("unshare failed")
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    #print("Logs from your program will appear here!")
-
     # Uncomment this block to pass the first stage
     #
     command = sys.argv[3]
@@ -26,17 +23,16 @@ def main():
     # Unshare the PID namespace
     unshare(CLONE_NEWPID)
 
+    # Fork the process
     pid = os.fork()
     if pid == 0:  # Child process
         os.chroot("/yinh")
         os.chdir("/")
-        completed_process = subprocess.run([command, *args], capture_output=True)
-        #print(completed_process.stdout.decode("utf-8"))
-        sys.stdout.buffer.write(completed_process.stdout)
-        sys.stderr.buffer.write(completed_process.stderr)
-        sys.exit(completed_process.returncode)
+        # Replace the current process with the target command
+        os.execvp(command, [command] + args)
     else:  # Parent process
-        os.waitpid(pid, 0)  # Wait for the child process to finish
+        _, status = os.waitpid(pid, 0)  # Wait for the child process to finish
+        sys.exit(os.WEXITSTATUS(status))  # Exit with the same status as the child
 
 if __name__ == "__main__":
     main()
